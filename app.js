9a6d70dc61b923d1880ec5b842564d942db291ed
brisseta <adrien.brisset3456@gmail.com>
2018-10-02 16:49:53 +0200

    feat(MongoDB) feat(RemoteDB) feat(test Bootstrap)

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var session = require('cookie-session'); // Charge le middleware de sessions
 var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-// var json = JSON.parse("dict.json");
+var mongoose = require('mongoose'),autoIncrement = require('mongoose-auto-increment');
+
+
 var app = express();
 
 /* On utilise les sessions */
@@ -24,8 +26,10 @@ on en crée une vide sous forme d'array avant la suite */
 })
 /* On ajoute un élément à la todolist */
 .post('/wup/add/', urlencodedParser, function(req, res) {
-    if (req.body.newtodo !== '') {
-        req.session.todolist.push(req.body.newtodo);
+    if (req.body.libelle !== '') {
+        var nouvelle_entry = new Entry(req.body.libelle);
+        nouvelle_entry.save_Entry_to_DB(this);
+        req.session.todolist.push(nouvelle_entry.toString());
     }
     res.redirect('/todo');
 })
@@ -42,4 +46,59 @@ on en crée une vide sous forme d'array avant la suite */
     res.redirect('/wup');
 })
 
-.listen(8080);   
+.listen(8080);
+
+class Entry {
+
+    constructor(libelle,ip,statut,SLA){
+        this._action = "à renseigner";
+        this._libelle = libelle;
+        this._ip = ip;
+        this._statut = statut;
+        this._SLA = SLA;
+    }
+
+
+    get libelle() {
+        return this._libelle;
+    }
+
+    get ip() {
+        return this._ip;
+    }
+
+    get statut() {
+        return this._statut;
+    }
+
+    get SLA() {
+        return this._SLA;
+    }
+
+    get action() {
+        return this._action;
+    }
+
+    set action(value) {
+        this._action = value;
+    }
+
+    save_Entry_to_DB(entry){
+        var db = mongoose.connect('mongodb://localhost:27017/WUP',{ useNewUrlParser: true });
+        autoIncrement.initialize(mongoose.connection);
+        var Shema_Entry = mongoose.Schema({
+            libelle : String,
+            ip : String,
+            statut : String,
+            sla : String,
+            action : String
+        });
+        Shema_Entry.plugin(autoIncrement.plugin,'Entry');
+        let Entry = mongoose.model('Entry',Shema_Entry);
+        var entry_tosave = new Entry(this.libelle,this.ip,this.statut,this.SLA,this.action);
+        entry_tosave.save(function (err) {
+           if(err) throw err;
+            console.log("entrée ajoutée" + entry_tosave);
+        });
+    }
+}
